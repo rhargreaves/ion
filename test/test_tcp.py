56b533aeb943a4fb14eb44057cb0ee7e2c3ca59d
Robert Hargreaves <kiyouta@gmail.com>
2025-10-06 22:31:40 +0100

    test: add basic TCP test

--- test/test_tcp.py
+++ test/test_tcp.py
@@ -1,3 +1,19 @@
+import subprocess
+import socket
+import time
+import os
 
 def test_tcp():
-    pass
+    server = subprocess.Popen(["./ion-server/cmake-build-debug/ion-server", "8080"])
+    time.sleep(1)
+
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect(("localhost", 8080))
+        data = sock.recv(1024).decode('utf-8')
+        assert data == "Hello TCP World"
+        sock.close()
+    finally:
+        server.terminate()
+        server.wait()
+
