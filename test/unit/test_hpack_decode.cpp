057daa3583c2eb1865a61af37f143d8936753275
Robert Hargreaves <kiyouta@gmail.com>
2025-11-08 21:15:53 +0000

    test: test boundaries of static table
    and fix array sizes

--- test/unit/test_hpack_decode.cpp
+++ test/unit/test_hpack_decode.cpp
@@ -3,29 +3,46 @@
 #include "header_block_decoder.h"
 #include "http2_frames.h"
 
+void check_header(std::vector<HttpHeader>& hdrs, size_t index, const std::string& expectedName,
+                  const std::string& expectedValue) {
+    REQUIRE(hdrs[index].name == expectedName);
+    REQUIRE(hdrs[index].value == expectedValue);
+};
+
 TEST_CASE("headers: decodes static table entries") {
-    std::array<uint8_t, 9> data = {0x82, 0x84, 0xbe, 0x87};
+    std::array<uint8_t, 4> data = {0x82, 0x84, 0xbe, 0x87};
 
     auto decoder = HeaderBlockDecoder{};
     auto hdrs = decoder.decode(data);
 
-    auto checkHeader = [&](size_t index, const std::string& expectedName,
-                           const std::string& expectedValue) {
-        REQUIRE(hdrs[index].name == expectedName);
-        REQUIRE(hdrs[index].value == expectedValue);
-    };
-
     REQUIRE(hdrs.size() == 3);  // skip over non-static table entries
 
     SECTION ("parses :method: GET") {
-        checkHeader(0, ":method", "GET");
+        check_header(hdrs, 0, ":method", "GET");
     }
 
     SECTION ("parses :path: /") {
-        checkHeader(1, ":path", "/");
+        check_header(hdrs, 1, ":path", "/");
     }
 
     SECTION ("parses :scheme: https") {
-        checkHeader(2, ":scheme", "https");
+        check_header(hdrs, 2, ":scheme", "https");
+    }
+}
+
+TEST_CASE("headers: decodes static table entries (boundaries)") {
+    std::array<uint8_t, 2> data = {0x81, 0xbd};
+
+    auto decoder = HeaderBlockDecoder{};
+    auto hdrs = decoder.decode(data);
+
+    REQUIRE(hdrs.size() == 2);
+
+    SECTION ("parses :authority: <empty>") {
+        check_header(hdrs, 0, ":authority", "");
+    }
+
+    SECTION ("parses www-authenticate: <empty>") {
+        check_header(hdrs, 1, "www-authenticate", "");
     }
 }
