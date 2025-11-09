fbab161736e699428140500453c92c20245a15c4
Robert Hargreaves <kiyouta@gmail.com>
2025-11-09 20:42:45 +0000

    fix: incorrect array size

--- test/unit/headers/test_header_block_decoder.cpp
+++ test/unit/headers/test_header_block_decoder.cpp
@@ -69,8 +69,8 @@ TEST_CASE("headers: decodes dynamic table entries") {
     auto decoder = ion::HeaderBlockDecoder{};
 
     SECTION ("stores and returns dynamic header with static header name") {
-        constexpr std::array<uint8_t, 11> req1 = {0x82, 0x84, 0x41, 0x86, 0xa0, 0xe4,
-                                                  0x1d, 0x13, 0x9d, 0x9,  0x87};
+        constexpr auto req1 = std::to_array<uint8_t>(
+            {0x82, 0x84, 0x41, 0x86, 0xa0, 0xe4, 0x1d, 0x13, 0x9d, 0x9, 0x87});
         auto hdrs1 = decoder.decode(req1);
 
         REQUIRE(hdrs1.size() == 4);
@@ -79,7 +79,7 @@ TEST_CASE("headers: decodes dynamic table entries") {
         check_header(hdrs1, 2, ":authority", "localhost");
         check_header(hdrs1, 3, ":scheme", "https");
 
-        constexpr std::array<uint8_t, 11> req2 = {0x82, 0x84, 0xbe, 0x87};
+        constexpr auto req2 = std::to_array<uint8_t>({0x82, 0x84, 0xbe, 0x87});
         auto hdrs2 = decoder.decode(req2);
 
         REQUIRE(hdrs1.size() == hdrs2.size());
