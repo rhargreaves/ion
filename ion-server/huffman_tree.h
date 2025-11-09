aab5666d89dce08c39686501fe3c431a31255b47
Robert Hargreaves <kiyouta@gmail.com>
2025-11-09 13:12:43 +0000

    refactor: update header file with param name change

--- ion-server/huffman_tree.h
+++ ion-server/huffman_tree.h
@@ -15,6 +15,6 @@ class HuffmanTree {
     HuffmanNode* root_ = new HuffmanNode();
 
    public:
-    void insert_symbol(int16_t symbol, uint32_t code, uint8_t bit_len);
+    void insert_symbol(int16_t symbol, uint32_t lsb_aligned_code, uint8_t bit_len);
     std::vector<int16_t> decode(std::span<const uint8_t> data, size_t bit_len);
 };
