76d2dd8b494c01dbfcdd541d2bef5ddd25c1cdd0
Robert Hargreaves <kiyouta@gmail.com>
2025-11-09 22:52:54 +0000

    refactor: break down main decode method further

--- lib/headers/header_block_decoder.cpp
+++ lib/headers/header_block_decoder.cpp
@@ -63,35 +63,43 @@ std::optional<HttpHeader> HeaderBlockDecoder::try_read_indexed_header(uint8_t in
     return dynamic_table_[dynamic_index];
 }
 
+std::optional<HttpHeader> HeaderBlockDecoder::try_decode_indexed_field(uint8_t first_byte) {
+    auto index = static_cast<uint8_t>(first_byte & 0x7F);
+    if (index < 1) {
+        spdlog::error("invalid header index (<1)");
+        return std::nullopt;
+    }
+    return try_read_indexed_header(index);
+}
+
+HttpHeader HeaderBlockDecoder::decode_literal_field(uint8_t first_byte, ByteReader& reader) {
+    auto index = static_cast<uint8_t>(first_byte & 0x3F);
+    bool is_new_name = index == 0;
+    auto name =
+        is_new_name ? read_length_and_string(reader) : std::string(STATIC_TABLE[index - 1].name);
+    auto value = read_length_and_string(reader);
+    auto hdr = HttpHeader{name, value};
+
+    // update dynamic table
+    dynamic_table_.push_back(hdr);
+
+    return hdr;
+}
+
 std::vector<HttpHeader> HeaderBlockDecoder::decode(std::span<const uint8_t> data) {
     auto hdrs = std::vector<HttpHeader>{};
     ByteReader reader(data);
 
     while (reader.has_bytes()) {
         uint8_t first_byte = reader.read_byte();
-
         if (first_byte & 0x80) {
             // indexed field (static or dynamic)
-            auto index = static_cast<uint8_t>(first_byte & 0x7F);
-            if (index < 1) {
-                spdlog::error("invalid header index (<1)");
-                continue;
-            }
-            if (auto hdr = try_read_indexed_header(index)) {
+            if (auto hdr = try_decode_indexed_field(first_byte)) {
                 hdrs.push_back(hdr.value());
             }
         } else if (first_byte & 0x40) {
             // literal header field
-            auto index = static_cast<uint8_t>(first_byte & 0x3F);
-            bool is_new_name = index == 0;
-            auto name = is_new_name ? read_length_and_string(reader)
-                                    : std::string(STATIC_TABLE[index - 1].name);
-            auto value = read_length_and_string(reader);
-            auto hdr = HttpHeader{name, value};
-            hdrs.push_back(hdr);
-
-            // update dynamic table
-            dynamic_table_.push_back(hdr);
+            hdrs.push_back(decode_literal_field(first_byte, reader));
         }
     }
     return hdrs;
