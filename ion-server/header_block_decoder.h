30ea44b0fb5808d2ca62c4782ee0ee7dba42b670
Robert Hargreaves <kiyouta@gmail.com>
2025-11-09 18:19:02 +0000

    refactor: move static table to code file

--- ion-server/header_block_decoder.h
+++ ion-server/header_block_decoder.h
@@ -7,15 +7,6 @@ struct HttpHeader {
     std::string value;
 };
 
-struct StaticHttpHeader {
-    std::string_view name;
-    std::string_view value;
-
-    [[nodiscard]] HttpHeader to_http_header() const {
-        return HttpHeader{std::string(name), std::string(value)};
-    }
-};
-
 class HeaderBlockDecoder {
    public:
     std::vector<HttpHeader> decode(std::span<const uint8_t> data);
