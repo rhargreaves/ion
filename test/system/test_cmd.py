f504201aa0284b66cb7d1cde637bdb7cc814cc5a
Robert Hargreaves <kiyouta@gmail.com>
2025-12-20 15:22:34 +0000

    test: all variants of help & version flags

--- test/system/test_cmd.py
+++ test/system/test_cmd.py
@@ -1,13 +1,15 @@
 from utils import run_server_custom_args
 import os
+import pytest
 
 
 def build_version():
     return os.environ.get("BUILD_VERSION")
 
 
-def test_displays_help():
-    result = run_server_custom_args(["--help"])
+@pytest.mark.parametrize("help_flag", ["--help", "-h"])
+def test_displays_help(help_flag):
+    result = run_server_custom_args([help_flag])
 
     help_text = result.stdout
     assert "Usage:" in help_text or "ion: the light-weight HTTP/2 server" in help_text
@@ -15,8 +17,9 @@ def test_displays_help():
     assert result.returncode == 0
 
 
-def test_displays_version():
-    result = run_server_custom_args(["--version"])
+@pytest.mark.parametrize("version_flag", ["--version", "-v"])
+def test_displays_version(version_flag):
+    result = run_server_custom_args([version_flag])
 
     assert result.returncode == 0
     assert build_version() in result.stdout
