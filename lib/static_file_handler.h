3a0e290a9ca6ff2c49b14cfd0f449aa8dc92a7e3
Robert Hargreaves <kiyouta@gmail.com>
2025-12-21 22:58:49 +0000

    chore: remove extraneous comments

--- lib/static_file_handler.h
+++ lib/static_file_handler.h
@@ -8,11 +8,7 @@ namespace ion {
 class StaticFileHandler {
    public:
     StaticFileHandler(const std::string& url_prefix, const std::string& filesystem_root);
-
-    // Check if this handler should handle the given path
     bool matches(const std::string& path) const;
-
-    // Handle the request
     HttpResponse handle(const std::string& path) const;
 
    private:
